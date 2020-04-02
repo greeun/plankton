1924cc24279c823c12c5f8175bdc795a99746650
greeun <github.com@tlog.net>
2020-04-02 20:29:04 +0900

    - implements class added.

---
+++ src/main/java/com/withwiz/plankton/network/client/netty/DefaultNettyTcpClient.java
@@ -0,0 +1,91 @@
+package com.withwiz.plankton.network.client.netty;
+
+import io.netty.channel.*;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Default TCP client class.<BR>
+ */
+public class DefaultNettyTcpClient extends AbstractNettyTcpClient {
+    /**
+     * loggger
+     */
+    private static Logger logger = LoggerFactory.getLogger(DefaultNettyTcpClient.class);
+
+    /**
+     * property: netty.nativeio
+     */
+    boolean isUseNativeIO = false;
+
+    /**
+     * server port
+     */
+    int port = 18080;
+
+    /**
+     * property: netty.threads.worker
+     */
+    int workerThreadSize = 10;
+
+    /**
+     * user handler
+     */
+    ChannelInboundHandlerAdapter handler = null;
+
+    /**
+     * constructor
+     */
+    public DefaultNettyTcpClient(int port, boolean isUseNativeIO) {
+        this.port = port;
+        this.isUseNativeIO = isUseNativeIO;
+    }
+
+    /**
+     * constructor
+     */
+    public DefaultNettyTcpClient(int workerThreadSize, int port, boolean isUseNativeIO) {
+        this(port, isUseNativeIO);
+        this.workerThreadSize = workerThreadSize;
+    }
+
+    public boolean isUseNativeIO() {
+        logger.debug("isUseNativeIO: {}", isUseNativeIO);
+        return isUseNativeIO;
+    }
+
+    public int getPort() {
+        logger.debug("port: {}", port);
+        return port;
+    }
+
+    public int getWorkerThreadSize() {
+        logger.debug("workerThreadSize: {}", workerThreadSize);
+        return workerThreadSize;
+    }
+
+    @Override
+    protected ChannelHandler getServiceHandler() {
+        return new ChannelInitializer<Channel>() {
+            @Override
+            protected void initChannel(Channel sc) {
+                ChannelPipeline cp = sc.pipeline();
+                cp.addLast(new LoggingHandler(LogLevel.DEBUG));
+//            cp.addLast(new ByteArrayDecoder());
+//            cp.addLast(new ByteArrayEncoder());
+                if(handler == null) {
+                    logger.error("User handler is NULL!");
+                } else {
+                    cp.addLast(handler);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void setHandler(ChannelInboundHandlerAdapter handler) {
+        this.handler = handler;
+    }
+}
---
+++ src/main/java/com/withwiz/plankton/network/client/netty/DefaultNettyUnixDomainSocketClient.java
@@ -0,0 +1,107 @@
+package com.withwiz.plankton.network.client.netty;
+
+import io.netty.channel.*;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Default UnixDomainSocket client class.<BR>
+ */
+public class DefaultNettyUnixDomainSocketClient extends AbstractNettyUnixDomainSocketClient {
+    /**
+     * loggger
+     */
+    private static Logger logger = LoggerFactory.getLogger(DefaultNettyUnixDomainSocketClient.class);
+
+    /**
+     * property: netty.uds
+     */
+    boolean isUseUds = true;
+
+    /**
+     * property: netty.nativeio
+     */
+    boolean isUseNativeIO = false;
+
+    /**
+     * property: netty.socket.path
+     */
+    String socketPath = "/tmp/uds.sock";
+
+    /**
+     * property: netty.threads.worker
+     */
+    int workerThreadSize = 10;
+
+    /**
+     * user handler
+     */
+    ChannelInboundHandlerAdapter handler = null;
+
+    /**
+     * constructor
+     */
+    public DefaultNettyUnixDomainSocketClient(boolean isUseUds, String socketPath, boolean isUseNativeIO) {
+        this.isUseUds = isUseUds;
+        this.socketPath = socketPath;
+        this.isUseNativeIO = isUseNativeIO;
+    }
+
+    /**
+     * constructor
+     */
+    public DefaultNettyUnixDomainSocketClient(int workerThreadSize, boolean isUseUds, String socketPath, boolean isUseNativeIO) {
+        this(isUseUds, socketPath, isUseNativeIO);
+        this.workerThreadSize = workerThreadSize;
+    }
+
+    public boolean isUseUds() {
+        logger.debug("isUds: {}", isUseUds);
+        return isUseUds;
+    }
+
+    public boolean isUseNativeIO() {
+        logger.debug("isUseNativeIO: {}", isUseNativeIO);
+        return isUseNativeIO;
+    }
+
+    public String getSocketPath() {
+        logger.debug("socketPath: {}", socketPath);
+        return socketPath;
+    }
+
+    public int getWorkerThreadSize() {
+        logger.debug("workerThreadSize: {}", workerThreadSize);
+        return workerThreadSize;
+    }
+
+    @Override
+    public boolean isUds() {
+        return true;
+    }
+
+    @Override
+    protected ChannelHandler getServiceHandler() {
+        return new ChannelInitializer<Channel>() {
+            @Override
+            protected void initChannel(Channel sc) {
+                ChannelPipeline cp = sc.pipeline();
+                cp.addLast(new LoggingHandler(LogLevel.DEBUG));
+//            cp.addLast(new ByteArrayDecoder());
+//            cp.addLast(new ByteArrayEncoder());
+                if(handler == null) {
+                    logger.error("User handler is NULL!");
+                } else {
+                    cp.addLast(handler);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void setHandler(ChannelInboundHandlerAdapter handler) {
+        this.handler = handler;
+    }
+}
