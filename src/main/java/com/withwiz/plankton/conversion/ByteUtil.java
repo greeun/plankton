65e63d035d94c3b05f9e7f5ef8428c3267b784df
greeun <github.com@tlog.net>
2020-07-31 17:57:57 +0900

    - class.method refactoring.

--- src/main/java/com/withwiz/plankton/conversion/ByteUtil.java
+++ src/main/java/com/withwiz/plankton/conversion/ByteUtil.java
@@ -1,7 +1,5 @@
 package com.withwiz.plankton.conversion;
 
-import java.nio.ByteBuffer;
-
 /**
  * byte utility class
  */
@@ -37,21 +35,6 @@ public class ByteUtil {
     }
 
     /**
-     * convert long to 8 byte array
-     *
-     * @param l long
-     * @return byte[]
-     */
-    public static byte[] toBytes(long l) {
-        byte[] result = new byte[8];
-        for (int i = 7; i >= 0; i--) {
-            result[i] = (byte) (l & 0xFF);
-            l >>= 8;
-        }
-        return result;
-    }
-
-    /**
      * convert 8 bytes to long
      *
      * @param ba byte[]
@@ -64,53 +47,6 @@ public class ByteUtil {
             result |= (ba[i] & 0xFF);
         }
         return result;
-    }
-
-    /**
-     * convert int to byte[]
-     *
-     * @param value           int value
-     * @param sizeOfByteArray size(2 or 4)
-     * @return byte[]
-     */
-    public static byte[] toByteArray(int value, int sizeOfByteArray) {
-        byte[] byteArray = new byte[sizeOfByteArray];
-        if (sizeOfByteArray == 2) {
-            byteArray[0] = (byte) value;
-            byteArray[1] = (byte) (value >>> 8);
-        } else if (sizeOfByteArray == 4) {
-            byteArray[0] = (byte) (value >> 24);
-            byteArray[1] = (byte) (value >> 16);
-            byteArray[2] = (byte) (value >> 8);
-            byteArray[3] = (byte) (value);
-        }
-        return byteArray;
-    }
-
-    /**
-     * convert int to byte[]
-     *
-     * @param value           int value
-     * @param sizeOfByteArray byte[] size
-     * @return byte[]
-     */
-    public static byte[] toByteArray2(int value, int sizeOfByteArray) {
-        byte[] ba = new byte[sizeOfByteArray];
-        for (int i = 0; i < sizeOfByteArray; i++) {
-            int offset = (ba.length - 1 - i) * 8;
-            ba[i] = (byte) ((value >>> offset) & 0xFF);
-        }
-        return ba;
-    }
-
-    /**
-     * convert int to byte[]
-     *
-     * @param value int value
-     * @return byte[]
-     */
-    public static byte[] toByteArray(int value) {
-        return ByteBuffer.allocate(4).putInt(value).array();
     }
 
     /* 4or2 bytearray  to int 변환*/
@@ -149,22 +85,6 @@ public class ByteUtil {
             value += (ba[i] & 0x000000FF) << shift;
         }
         return value;
-    }
-
-    /* shot to byte 변환 */
-
-    /**
-     * conver short to bytes[]
-     *
-     * @param Value short value
-     * @param order big(1) or little endian(0)
-     * @return byte[]
-     */
-    public static byte[] toByteArray(short Value, int order) {
-        byte[] temp;
-        temp = new byte[]{(byte) ((Value & 0xFF00) >> 8), (byte) (Value & 0x00FF)};
-        temp = byteOrder(temp, order);
-        return temp;
     }
 
     /**
@@ -242,7 +162,7 @@ public class ByteUtil {
         int i = 100;
         System.out.println("value: " + i + " --> " + IntUtil.toHexString(i));
         System.out.println("value: " + i + " --> " + IntUtil.toHexString(i));
-        System.out.println("value: " + i + " --> " + ByteUtil.toHexString(ByteUtil.toByteArray(i, 4)));
-        System.out.println("value: " + i + " --> " + ByteUtil.toHexString(ByteUtil.toByteArray2(i, 4)));
+        System.out.println("value: " + i + " --> " + ByteUtil.toHexString(IntUtil.toByteArray(i, 4)));
+        System.out.println("value: " + i + " --> " + ByteUtil.toHexString(IntUtil.toByteArray2(i, 4)));
     }
 }
--- src/main/java/com/withwiz/plankton/conversion/IntUtil.java
+++ src/main/java/com/withwiz/plankton/conversion/IntUtil.java
@@ -1,5 +1,7 @@
 package com.withwiz.plankton.conversion;
 
+import java.nio.ByteBuffer;
+
 /**
  * integer utility class
  */
@@ -33,5 +35,68 @@ public class IntUtil {
      */
     public static String toHexString(int value, int digit) {
         return String.format("%0" + (digit * 2) + "X", value);
+    }
+
+    /**
+     * convert int to byte[]
+     *
+     * @param value           int value
+     * @param sizeOfByteArray size(2 or 4)
+     * @return byte[]
+     */
+    public static byte[] toByteArray(int value, int sizeOfByteArray) {
+        byte[] byteArray = new byte[sizeOfByteArray];
+        if (sizeOfByteArray == 2) {
+            byteArray[0] = (byte) value;
+            byteArray[1] = (byte) (value >>> 8);
+        } else if (sizeOfByteArray == 4) {
+            byteArray[0] = (byte) (value >> 24);
+            byteArray[1] = (byte) (value >> 16);
+            byteArray[2] = (byte) (value >> 8);
+            byteArray[3] = (byte) (value);
+        }
+        return byteArray;
+    }
+
+    /**
+     * convert int to byte[]
+     *
+     * @param value           int value
+     * @param sizeOfByteArray byte[] size
+     * @return byte[]
+     */
+    public static byte[] toByteArray2(int value, int sizeOfByteArray) {
+        byte[] ba = new byte[sizeOfByteArray];
+        for (int i = 0; i < sizeOfByteArray; i++) {
+            int offset = (ba.length - 1 - i) * 8;
+            ba[i] = (byte) ((value >>> offset) & 0xFF);
+        }
+        return ba;
+    }
+
+    /**
+     * convert int to byte[]
+     *
+     * @param value int value
+     * @return byte[]
+     */
+    public static byte[] toByteArray(int value) {
+        return ByteBuffer.allocate(4).putInt(value).array();
+    }
+
+    /* shot to byte 변환 */
+
+    /**
+     * conver short to bytes[]
+     *
+     * @param Value short value
+     * @param order big(1) or little endian(0)
+     * @return byte[]
+     */
+    public static byte[] toByteArray(short Value, int order) {
+        byte[] temp;
+        temp = new byte[]{(byte) ((Value & 0xFF00) >> 8), (byte) (Value & 0x00FF)};
+        temp = ByteUtil.byteOrder(temp, order);
+        return temp;
     }
 }
